49b34b22be5da8bc7b6711f73cb9806cff7ae67e
kaforad <kaforad@gmail.com>
2018-10-24 23:39:41 +0100

    query for third report re-written code refactor

--- kaf_loganalysis.py
+++ kaf_loganalysis.py
@@ -1,103 +1,117 @@
+#!/usr/bin/env python2.7
 import psycopg2
 POPULAR_ARTICLE = "\n\n MOST POPULAR THREE ARTICLES OF ALL TIME \n "
 POPULAR_AUTHOR = "\n\n MOST POPULAR ARTICLE AUTHORS OF ALL TIME \n "
-POPULAR_ERROR = "\n\n DAYS WHEN 1% of REQUESTS LEAD TO  ERRORS \n "
+POPULAR_ERROR = "\n\n DAYS WHEN MORE THAN 1% of\
+REQUESTS LEAD TO  ERRORS \n "
+DASH_LINE = "_" * 50 + "\n\n"
 
 
 def writeToFile(report, reportType):
     # Write report output to text file
     # create a text file for writing and updating data into.
-    printReport = open("report.txt", "a+")
-    emptyStri = " "
-    # loop through list to concantenate report string
-    printReport.write(reportType)
-    printReport.write("_________________________________________\n")
-    for value in report.values():
+    with open("report.txt", "a+") as printReport:
         emptyStri = " "
-        for data in value:
-            emptyStri += str(data)
-        # write line to output file
-        printReport.write(emptyStri + "\n")
-        printReport.write("\n")
-    printReport.close()
-    # establish connection
+        # loop through list to concantenate report string
+        printReport.write(reportType)
+        printReport.write(DASH_LINE)
+        for value in report.values():
+            emptyStri = " "
+            for data in value:
+                emptyStri += str(data)
+            # write line to output file
+            printReport.write(emptyStri + "\n")
+            printReport.write("\n")
+        printReport.close()
 
 
 def articlePopularity():
-	""""Function  outputs the most popular three articles of all time from a news database that lead to errors"""
-	conn = psycopg2.connect(dbname="news")
-	cursor = conn.cursor()
-	myquery = """select title, logs.popularity from articles join (select reverse(split_part(reverse(path), '/', 1)) as path, count(*) as popularity from log  group by path having path like '%article%'order by popularity  desc limit 3) as logs on articles.slug = logs.path order by logs.popularity desc"""
-	cursor.execute(myquery)
-	results = cursor.fetchall()
-	report = {}
-	i = 0
-	print(POPULAR_ARTICLE)
-	print("_________________________________________\n")
-	for result in results:
-		i = i + 1 
-		print result[0], "____", result[1], "views \n"
-		report[i] = result[0], "____", result[1], "views"
-	print("\n\n")
-	writeToFile(report, POPULAR_ARTICLE)
-	conn.close()
+    """"Function  outputs the most popular three articles of all time\
+    from a news database that lead to errors"""
+    conn = psycopg2.connect(dbname="news")
+    cursor = conn.cursor()
+    myquery = """select title, logs.popularity from articles join
+     (select reverse(split_part(reverse(path), '/', 1)) as
+     path,count(*) as popularity from log
+     group by path having path like
+     '%article%'order by popularity  desc limit 3) as logs on
+     articles.slug = logs.path order by logs.popularity desc"""
+    cursor.execute(myquery)
+    results = cursor.fetchall()
+    report = {}
+    i = 0
+    print(POPULAR_ARTICLE)
+    print(DASH_LINE)
+    for result in results:
+        i = i + 1
+        print result[0], "____", result[1], "views \n"
+        report[i] = result[0], "____", result[1], "views"
+    print("\n\n")
+    writeToFile(report, POPULAR_ARTICLE)
+    conn.close()
 
 
 def authorPopularity():
-	""""Function  outputs the most popular article authors of all time 	from a news database that lead to errors		"""
-	conn = psycopg2.connect(dbname="news")
-	cursor = conn.cursor()
-	myquery = """ select authors.name as Author, sum(pTitle.popularity) as Popularity from 
-    authors join (select author,title from articles group by author,title) as 
-    Aarticles on authors.id= Aarticles.author join 
-    (Select title, logs.popularity from articles join
-    (select reverse(split_part(reverse(path), '/', 1)) as path, 
-    count(*) as popularity from log  group by path having 
-    path like '%article%'order by popularity  desc ) as logs  
-    on articles.slug = logs.path order by logs.popularity desc) as pTitle 
-    on pTitle.title =Aarticles.title 
-    group by authors.name order by Popularity desc  """
-	cursor.execute(myquery)
-	results = cursor.fetchall()
-	report = {}
-	i = 0
-	print(POPULAR_AUTHOR)
-	print("_________________________________________\n")
-	for result in results:
-		i = i + 1
-		print result[0], "____", result[1], "views \n"
-		report[i] = result[0], "____", result[1], "views"
-	print("\n\n")
-	writeToFile(report, POPULAR_AUTHOR)
-	print("\n\n")
-	conn.close()
+    """"Function  outputs the most popular article authors of all time\
+    from a news database that lead to errors        """
+    conn = psycopg2.connect(dbname="news")
+    cursor = conn.cursor()
+    myquery = """ SELECT VIEW_ArticlesAuthor.author,sum(popularity) as
+    popularity from VIEW_ArticlesAuthor join VIEW_ArticleLog on
+    VIEW_ArticlesAuthor.title =VIEW_ArticleLog.title
+    group by VIEW_ArticlesAuthor.author
+    order by Popularity desc; """
+    cursor.execute(myquery)
+    results = cursor.fetchall()
+    report = {}
+    i = 0
+    print(POPULAR_AUTHOR)
+    print(DASH_LINE)
+    for result in results:
+        i = i + 1
+        print result[0], "____", result[1], "views \n"
+        report[i] = result[0], "____", result[1], "views"
+    print("\n\n")
+    writeToFile(report, POPULAR_AUTHOR)
+    print("\n\n")
+    conn.close()
 
 
 def errorPopularity():
-	""""Function outputs more 1% of articles requests from a news database that lead to errors	"""
-	conn = psycopg2.connect(dbname="news")
-	cursor = conn.cursor()
-	myquery = """select TO_CHAR(time, 'FMMonth FMDDth, YYYY') as time, 
-	(ROUND(count(*)*100.0/(select count(*) from log where status<>'200 OK' group by status),2)) as percent 
-	from log where status<>'200 OK' group by TO_CHAR(time, 'FMMonth FMDDth, YYYY'), method, status 
-	having (count(*)*100.0/(select count(*) from log where status<>'200 OK' group by status))>1 order by time desc ;  """
-	cursor.execute(myquery)
-	results = cursor.fetchall()
-	report = {}
-	i = 0
-	print(POPULAR_ERROR)
-	print("_________________________________________\n")
-	for result in results:
-		i = i + 1
-		print result[0], "____", result[1], "% \n"
-		report[i] = result[0], "____", result[1], "%"
-	print("\n\n")
-	writeToFile(report, POPULAR_ERROR)
-	print("\n\n")
-	conn.close()
+    """"Function outputs more 1% of articles requests from a news
+    database that lead to errors    """
+    conn = psycopg2.connect(dbname="news")
+    cursor = conn.cursor()
+    myquery = """WITH errCount AS(
+                    Select * from VIEW_DailyErrCount
+                ),totalRequest As(
+                    select  errCount.count as count ,
+                    cast(errCount.time as date),total.count as total,
+                    ROUND((errCount.count*100/total.count),2) as PERCENT
+                    from errCount join
+                    (Select * from VIEW_TotalDailyRequest) as total on
+                        cast(errCount.time as date)= cast(total.time as date)
 
-articlePopularity()
-authorPopularity()
-errorPopularity()
+                )
+                select TO_CHAR(time, 'FMMonth FMDDth, YYYY') as time,percent
+                from totalRequest where percent>1;  """
+    cursor.execute(myquery)
+    results = cursor.fetchall()
+    report = {}
+    i = 0
+    print(POPULAR_ERROR)
+    print(DASH_LINE + "\n\n")
+    for result in results:
+        i = i + 1
+        print result[0], "____", result[1], "% \n"
+        report[i] = result[0], "____", result[1], "%"
+    print("\n\n")
+    writeToFile(report, POPULAR_ERROR)
+    print("\n\n")
+    conn.close()
 
-			
+if __name__ == '__main__':
+
+    articlePopularity()
+    authorPopularity()
+    errorPopularity()
